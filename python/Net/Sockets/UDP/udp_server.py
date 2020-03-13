f9951267c40fc38a42b6672520e461d4a5f192ff
MaximK <m.klimovich76@gmail.com>
2020-03-13 18:21:01 +1100

    UDP server/client

---
+++ python/Net/Sockets/UDP/udp_server.py
@@ -0,0 +1,23 @@
+import socket
+import sys
+
+if len(sys.argv) != 3:
+    print('Usage: udpserver.py localIP localPort')
+    exit()
+
+# Create UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to the port
+server_address = (sys.argv[1], int(sys.argv[2]))
+print('Starting UDP server on %s port %s' % server_address)
+sock.bind(server_address)
+
+while True:
+    data, address = sock.recvfrom(4096)
+    
+    print('Received %s bytes from %s' % (len(data), address))
+    
+    if data:
+        sent = sock.sendto(data, address)
+        print('Rent %s bytes back to %s' % (sent, address))
